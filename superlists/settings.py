795784bbd52dad319b7546237335fccb6cd8dc83
Hayley Mathews <yymath@localhost.localdomain>
2015-12-30 09:14:37 -0600

    Refactor home page view to use a template

--- superlists/settings.py
+++ superlists/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'lists',
 )
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
