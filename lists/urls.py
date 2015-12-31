4cfc85898db73b1f3d2f96b3199764ef28b3c5dc
Hayley Mathews <yymath@localhost.localdomain>
2015-12-31 12:55:18 -0600

    clean up URLs

--- lists/urls.py
+++ lists/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from lists import views as list_views
-from lists import urls as lists_urls
+from lists import views
 
 urlpatterns = [
-	url(r'^$', lists_views.home_page, name='home'),
-	url(r'^lists/', include(list_urls)),
-   #  url(r'^admin/', include(admin.site.urls)),
+	url(r'^lists/new$', views.new_list, name='new_list'),
+	url(r'^lists/(\d+)/$', views.view_list, name = 'view_list'),
+	url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
 ]
